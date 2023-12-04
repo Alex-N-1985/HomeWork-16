31091cb755da03156f8e5b163908f29c6f4a7bef
Alex <42239536+RobertAnderson1985@users.noreply.github.com>
2023-12-04 21:50:27 +0300

    обновление кода

--- js/task5.js
+++ js/task5.js
@@ -1,11 +1,14 @@
 function btnClick(){
     var time = Math.floor(Math.random() * (2592000000 - 86400000)) + 86400000 ;
-    var ms = time,
-        sec = Math.floor(ms / 1000);
-        min = Math.floor(sec / 60);
-        hour = Math.floor(min / 60);
-        day = Math.floor(hour / 24);
-    var output = "Суток: " + day + "\n Часов: " + hour + "\n Минут: " + min + "\n Секунд: " + sec + "\n Милисекунд: " + ms
+    var day = Math.floor(time / 86400000),
+        res = time % 86400000;
+    var hour = Math.floor(res / 3600000),
+        res = res % 3600000;
+    var min = Math.floor(res / 60000),
+        res = res % 60000;
+        sec = Math.floor(res / 1000);
+        ms = res % 1000;
+    var output = "Всего: " + time +  "\n Суток: " + day + "\n Часов: " + hour + "\n Минут: " + min + "\n Секунд: " + sec + "\n Милисекунд: " + ms
     document.getElementById("output").innerText = output;
     console.log(output);
 }
